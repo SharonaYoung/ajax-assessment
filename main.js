c30a882850f1a7a5cc64e4115799d454ddb5e77e
Sharona Young <msrona806@gmail.com>
2023-02-09 02:12:52 -0500

    added notes

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
-const user = document.querySelector("#user");
+// variables for html elements
+const inputEl = document.querySelector("#input");
 const search = document.querySelector("#search");
 
 // add event listener for submit button
 document.querySelector('form').addEventListener('submit', function(event){
   event.preventDefault();
-  console.log('button clicked');
   // get the value from the input
   console.log(user.value);
   // clear the input box
   user.value = " ";
 });
+
+// fetch data from github api using async function
+
+// function to render profile information
