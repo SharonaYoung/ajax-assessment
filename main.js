471b2b51f026a6b1f85447bcab52cc9bdce17702
Sharona Young <msrona806@gmail.com>
2023-02-09 02:28:57 -0500

    fetch to api is working

--- main.js
+++ main.js
@@ -1,16 +1,26 @@
 // variables for html elements
-const inputEl = document.querySelector("#input");
-const search = document.querySelector("#search");
+const inputEl = document.querySelector('#input');
+const search = document.querySelector('#search');
+const form = document.querySelector('form');
 
 // add event listener for submit button
-document.querySelector('form').addEventListener('submit', function(event){
-  event.preventDefault();
-  // get the value from the input
-  console.log(user.value);
-  // clear the input box
-  user.value = " ";
-});
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    // get the value from the input
+    console.log(inputEl.value);
+    // clear the input box
+    inputEl.value = " ";
+  });
 
 // fetch data from github api using async function
+async function fetchProfile() {
+  try{
+    const res = await fetch(`https://api.github.com/users/sharonayoung`);
+    const data = await res.json();
+    console.log(data);
+  }
+  catch{}
+}
+fetchProfile();
 
 // function to render profile information
